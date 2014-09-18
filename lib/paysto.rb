69dbc77616d0f43cd0ec8574ad9566ac5bde268b
Artem Harmaty <harmaty@gmail.com>
2014-09-18 23:36:14 +0400

    refactored methods

--- lib/paysto.rb
+++ lib/paysto.rb
@@ -1,17 +1,14 @@
 class Paysto
-  PAYMENT_GATE_URL = 'https://paysto.com/api/paymentGate/'
-  API_URL = 'https://paysto.com/api/Common/'
-  PAYMENT_STATUS_URL = 'https://paysto.com/api/Payment/GetByInvoiceId'
+  API_URL = 'https://paysto.com'
 
   def initialize(shop_id, secret)
     @shop_id = shop_id
     @secret = secret
+    @uri = URI('https://paysto.com')
   end
 
   def create_request(type, options)
-    params = {PAYSTO_SHOP_ID: @shop_id}.merge(options)
-    params[:PAYSTO_MD5] = sign params
-    response = api_call "#{PAYMENT_GATE_URL}/#{type}", params
+    response = api_call "/api/paymentGate/#{type}", options
     status, message = response.split ','
     if status != 'RES_RESERVED'
       raise PaystoException, "#{status}: #{message}"
@@ -20,45 +17,45 @@ class Paysto
   end
 
   def check_payment_status(invoice_id)
-    params = {
-        PAYSTO_SHOP_ID: @shop_id,
-        PAYSTO_INVOICE_ID: invoice_id,
-        PAYSTO_REQUEST_NO: Time.now.to_i
+    options = {
+        PAYSTO_INVOICE_ID: invoice_id
     }
-    params[:PAYSTO_MD5] = sign params
-    response = api_call PAYMENT_STATUS_URL, params
-
+    response = api_call '/api/Payment/GetByInvoiceId', options
+    data = response.split ','
+    if data[0] == 'RES_ERROR'
+      raise PaystoException, "#{data[0]}: #{data[1]}"
+    end
+    data.try :[], 5
   end
 
   def balance
-    params = {
-        PAYSTO_SHOP_ID: @shop_id,
-        PAYSTO_REQUEST_NO: Time.now.to_i
-    }
-    params[:PAYSTO_MD5] = sign params
-    response = api_call "#{API_URL}/Balance", params
+    response = api_call "/api/Common/Balance"
     response.to_f
   end
 
   private
 
-  def api_call(url, params)
-    uri = URI(url)
-    http = Net::HTTP.new(uri.host, uri.port)
+  def api_call(path, options = {})
+    @uri.path = path
+    http = Net::HTTP.new(@uri.host, @uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.set_form_data(params)
+    request = Net::HTTP::Post.new(@uri.request_uri)
+    request.set_form_data(prepare_params(options))
     response = http.request(request)
     response.body.force_encoding 'UTF-8'
+  end
+
+  def prepare_params(options = {})
+    params = {PAYSTO_SHOP_ID: @shop_id}.merge options
+    params[:PAYSTO_REQUEST_NO] = Time.now.to_i
+    params[:PAYSTO_MD5] = sign params
+    params
   end
 
   def sign(params)
     q = params.sort.map{ |k, v| "#{k}=#{v}" }.join('&')
     Digest::MD5.hexdigest(q + "&#{@secret}").upcase
   end
+
 end
-
-
-
-
